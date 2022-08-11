fef774f93c63a7a27ce4d29430b75af16362b355
Andrea Naldini <naldadoc@Mini-di-Andrea.fritz.box>
2022-08-12 00:11:58 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,53 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+#imports
+from  kivymd.app import MDApp
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import Screen,ScreenManager
+from kivymd.uix.list import MDList
+from kivymd.uix.bottomnavigation import MDBottomNavigation
+
+class  General_UIApp(MDApp):
+
+    def build(self):
+
+        self.theme_cls.primary_palette = 'Red'
+        self.theme_cls.accent_palette = 'BlueGray'
+        self.theme_cls.theme_style = 'Light'
+        Builder.load_file('User_interface.kv')
+        Builder.load_file('Start_screen.kv')
+        Builder.load_file('NavDrawerContent.kv')
+        Builder.load_file('Instrument_config.kv')
+        return User_interface()
+
+    pass
+
+class User_interface(BoxLayout):
+    pass
+
+class  Start_Screen(Screen):
+    pass
+
+class Instrument_config(Screen):
+    pass
+
+class NavDrawerContent(MDList):
+    pass
+
+
+class Instrument_config(Screen):
+    pass
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+    General_UIApp().run()
+
+    pass
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
